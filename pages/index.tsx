8ae65a7d717fdd68026de61253390fd69f935774
Diego Alejandro Poveda <alejopov@gmail.com>
2023-05-14 11:23:23 -0500

    feat: add session validation to graphql server

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,13 @@
-const Home = () => <h1>Hello world</h1>;
+import { signIn, signOut } from 'next-auth/react';
+
+const Home = () => (
+  <>
+    <h1>Hello world</h1>
+    <button onClick={() => signIn()}>Iniciar sesión</button>
+    <button type='button' onClick={() => signOut()}>
+      Log out
+    </button>
+  </>
+);
 
 export default Home;
