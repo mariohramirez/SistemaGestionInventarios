0675aaac6e85c2a299fa9b98368ead371aaae998
mariohramirez <marioh.ramirez@udea.edu.co>
2023-05-15 01:34:12 -0500

    Creado sidebar

--- components/Inventario.tsx
+++ components/Inventario.tsx
@@ -6,29 +6,12 @@ import inventario2 from '../public/inventario2.png';
 import cart from '../public/cart.png';
 import user from '../public/user.png';
 import card from '../public/card.png';
+import Sidebar from './Sidebar';
 
 const Inventario = () => {
     return (
         <div className='bg-[#EAF3D8] h-[1024px] w-[1440px]'>
-            <div className='bg-[#004737] h-[1024px] w-[292px]'>
-                <div className='grid justify-items-center'>
-                    <Image src={picture} alt='Picture' className='pt-[49px]' />
-                    <div className='pt-[18px] text-white text-xl font-poppins'>Ava Williams</div>
-                    <div className='pt-[65px]'></div>
-                    <button className='bg-[#5EFEA9] h-[65px] w-[226px] rounded-xl text-xl font-poppins'>
-                        <div className='flex justify-center'><Image src={inventario2} alt='Inventario' className='ml-[-60px] mr-[10px]' />Inventarios</div>
-                    </button>
-                    <div className='pt-[65px]'></div>
-                    <button className='h-[65px] w-[226px] rounded-xl text-white text-xl font-poppins'>
-                        <div className='flex justify-center'><Image src={cart} alt='cart' className='ml-[-60px] mr-[10px]' />Materiales</div>
-                    </button>
-                    <div className='pt-[65px]'></div>
-                    <button className='h-[65px] w-[226px] rounded-xl text-white text-xl font-poppins'>
-                        <div className='flex justify-center'><Image src={user} alt='user' className='ml-[-60px] mr-[10px]' />Usuarios</div>
-                    </button>
-                    <Image src={logo} alt='Logo' className='pl-[47px] pr-[47px] pt-[280px]' />
-                </div>
-            </div>
+            <Sidebar></Sidebar>
             <div className='pl-[292px] mt-[-930px] w-[1440px] grid justify-items-center'>
                 <div className='w-[460px] text-center text-5xl font-poppins'>Inventarios</div>
                 <div className='pt-[77px] flex space-x-[526px]'>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Login from '@components/Login/Login'
+import Inventario from '@components/Inventario'
 
 export default function Home() {
     return (
         <>
             <main>
-                <Login></Login>
+                <Inventario></Inventario>
             </main>
         </>
     )
