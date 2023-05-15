5d2bb864fafa0ee2ec04d83422ba65d518c61ef9
pipesanta <juansantag95@gmail.com>
2023-05-14 23:39:20 -0500

    login responsivo

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,26 +1,61 @@
 import React from 'react'
 import logo from '../public/logo.png';
 import Image from 'next/image';
+import { BreakPoint } from 'pages/components/breakpoint';
+
+const inputClasses = "w-full rounded-xl placeholder-[#EAF3D8] placeholder:italic placeholder:pl-[20px] placeholder:text-lg h-12 mt-2";
 
 const Login = () => {
     return (
-        <div className='bg-[#EAF3D8] h-[1024px] w-[1440px]'>
+        <div className='bg-[#EAF3D8] h-screen w-full border border-red-600 flex flex-col  md:flex-row'>
 
-            <div className='bg-[#004737] h-[1024px] w-[720px]'>
+
+            {/*  Left side on desktop screems */}
+            <div className='bg-[#004737] w-full max-w-3xl md:w-1/2 px-3 py-6 border border-red-600 flex flex-col justify-center'>
                 <div className='grid justify-items-center'>
-                    <Image src={logo} alt='Logo' className='pt-[335px]' />
-                    <div className='h-[116px] w-[494px] pt-[166px] text-white text-center text-5xl font-poppins'>Sistema de gestión de inventarios</div>
+                    <Image src={logo} alt='Logo' />
+                    <div className='text-white text-center text-3xl lg:text-5xl font-poppins pt-6'>
+                        Sistema de gestión de inventarios
+                    </div>
                 </div>
             </div>
-            <div className='pl-[866px] mt-[-791px] w-[460px] h-[1000px]'>
-                <div className='w-[460px] text-center text-5xl font-poppins '>Iniciar Sesión</div>
-                <div className='pt-[142px] pb-[12px] text-xl font-poppins'>Correo</div>
-                <input className='h-[65px] w-[460px] rounded-xl placeholder-[#EAF3D8] placeholder:italic placeholder:pl-[20px] placeholder:text-lg' type="Correo" placeholder="Ingrese su correo" />
-                <div className='pt-[33px]  pb-[12px] text-xl font-poppins'>Contraseña</div>
-                <input className='h-[65px] w-[460px] rounded-xl placeholder-[#EAF3D8] placeholder:italic placeholder:pl-[20px] placeholder:text-lg' type="Contraseña" placeholder="Ingrese su contraseña" />
-                <div className='pt-[122px]'></div>
-                <button className='bg-[#004737] h-[65px] w-[460px] rounded-xl text-xl text-white font-poppins'>Iniciar Sesión</button>
+
+            <div className='w-full md:w-1/2 pt-6 border border-red-600 flex flex-col justify-center px-8 items-center'>
+
+                {/* <BreakPoint></BreakPoint> */}
+
+                {/* LABEL */}
+                <div className='text-center text-3xl lg:text-5xl font-poppins '>
+                    Iniciar Sesión
+                </div>
+
+
+                <div className='w-full max-w-lg mb-4 '>
+                    <div className='text-xl font-poppins'>Correo</div>
+                    <input className={`${inputClasses}`}
+                        type="email"
+                        placeholder="Ingrese su correo"
+                    />
+                </div>
+
+
+                <div className='w-full max-w-lg'>
+                    <div className='text-xl font-poppins'>Contraseña</div>
+                    <input className={`${inputClasses}`}
+                        type="password"
+                        placeholder="Ingrese su contraseña"
+                    />
+                </div>
+
+
+                <div className='w-full max-w-lg mt-6'>
+                    <button className='bg-[#004737] rounded-xl text-xl text-white font-poppins w-full h-12'>
+                        Iniciar Sesión
+                    </button>
+                </div>
+
             </div>
+
         </div>
     )
 
