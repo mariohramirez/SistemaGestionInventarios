0a1f01e7dbd053113bd17cacb3c49a18569da895
mariohramirez <marioh.ramirez@udea.edu.co>
2023-05-16 11:53:20 -0500

    Cambios

--- graphql/server/resolvers.ts
+++ graphql/server/resolvers.ts
@@ -2,13 +2,15 @@ import { Resolver } from 'types';
 import { DATE_LOCALE, DATE_OPTIONS } from '@config/date';
 
 const resolvers: Resolver = {
+
   User: {
-    role: async (parent, args, { db }) =>
-      await db.role.findUnique({
+    rol: async (parent, args, context) => {
+      return await context.db.role.findUnique({
         where: {
-          id: parent.roleId,
+          id: parent.rolId,
         },
-      }),
+      })
+    }
   },
   Material: {
     createdAt: (parent, args, context) =>
@@ -16,43 +18,50 @@ const resolvers: Resolver = {
   },
   Query: {
     users: async (parent, args, { db }) => await db.user.findMany(),
-    materials: async (parent, args, { db, session }) => {
-      if (!session) return null;
-
-      return await db.material.findMany();
-    },
-    materialsByUser: async (parent, args, { db, session }) => {
-      if (!session) return null;
-      const email = session?.user?.email ?? '';
-
-      return await db.material.findMany({
+    user: async (parent, args, { db }) => await db.user.findUnique({ where: { id: args.id }}),
+    materials: async (parent, args, { db }) => await db.material.findMany(),
+    materialsByUser: async (parent, { user }, { db }) =>
+      await db.material.findMany({
         where: {
-          createdBy: {
-            email: email,
-          },
+          createdById: user,
         },
-      });
-    },
+      }),
   },
   Mutation: {
-    createMaterial: async (parent, { name, price }, { db, session }) => {
-      if (!session) return null;
-
-      const email = session?.user?.email ?? '';
-
-      return await db.material.create({
+    createMaterial: async (parent, { name, price, user }, { db }) =>
+      await db.material.create({
         data: {
           name,
           price,
-          createdBy: {
-            connect: {
-              email: email,
-            },
-          },
+          createdById: user,
         },
-      });
+      }),
+    //Crear un nuevo usuario
+    createUser(parent, args, { db }) {
+      return db.user.create({ data: args });
     },
+
+    // Actualizar un usuario existente
+    updateUser(parent, args, { db }) {
+      return db.user.update({ where: { id: args.id }, data: args });
+    },
+
+    // Eliminar un usuario existente
+    deleteUser(parent, args, { db }) {     
+      return db.user.delete({ where: { id: args.id } });
+    },
+    
+    // Actualizar el rol
+    updateUserRole(parent, args, { db }) {      
+      return db.user.update({
+        where: { id: args.id },
+        data: { role: args.role },
+      }); 
+    },  
+
   },
+
+
 };
 
 export { resolvers };
--- graphql/server/types.ts
+++ graphql/server/types.ts
@@ -3,18 +3,17 @@ import gql from 'graphql-tag';
 const typeDefs = gql`
   scalar DateTime
 
-  type Role {
-    id: ID
-    name: String
-    users: [User]
-  }
+  enum Role {
+  USER
+  ADMIN
+}
 
   type User {
     id: ID
     name: String
     email: String
     image: String
-    role: Role
+    role:Role
   }
 
   type Material {
@@ -25,14 +24,19 @@ const typeDefs = gql`
   }
 
   type Query {
-    users: [User]
+    users: [User]!
+    user(id: ID!): User!
+    user(email:String!): User!
     materials: [Material]
-    materialsByUser: [Material]
+    materialsByUser(user: ID!): [Material]
   }
 
   type Mutation {
+    updateUser(id: ID!, name: String, email: String): User
     createUser(name: String!, email: String!, password: String!): User
-    createMaterial(name: String!, price: Int!): Material
+    deleteUser(id: ID!): Boolean
+    updateUserRole(id: ID!, role: Role!): User
+    createMaterial(name: String!, price: Int!, user: ID!): Material
   }
 `;
 
